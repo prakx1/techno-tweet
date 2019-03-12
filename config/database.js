957de7765b67d3131db2428808ec3ed3e6f249d4
prakx1 <prakx2050@gmail.com>
2019-03-12 19:42:27 +0530

    prakx

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb+srv://phenom:1708@technotest-a8hf3.mongodb.net/test?retryWrites=true',
+    database: 'mongodb+srv://prakx1:Prakash0402@cluster0-o1skj.mongodb.net/test?retryWrites=true',
     secret: 'phenom'
 }
